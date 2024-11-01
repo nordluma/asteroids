ca388411eaa6fc7fb5c67366377b4054661e1b47
nordluma <maxnordlund.95@hotmail.com>
2024-11-01 20:51:43 +0200

    feat: create a game clock and calculate delta time

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         screen.fill(color="black")
         pygame.display.flip()
@@ -12,6 +16,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        elapsed_time_ms = game_clock.tick(60)
+        dt = elapsed_time_ms / 1000  # convert from ms to sec
 
 
 if __name__ == "__main__":
