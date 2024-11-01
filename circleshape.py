e9267ccf42ec1fd23414f7174e489067f282c3a2
nordluma <maxnordlund.95@hotmail.com>
2024-11-01 21:48:47 +0200

    fix(circleshape): change type in type hints

--- circleshape.py
+++ circleshape.py
@@ -2,7 +2,7 @@ import pygame
 
 
 class CircleShape(pygame.sprite.Sprite):
-    def __init__(self, x: int, y: int, radius: int):
+    def __init__(self, x: float, y: float, radius: int):
         if hasattr(self, "containers"):
             super().__init__(self.containers)
         else:
